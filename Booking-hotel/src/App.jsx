a080ff02cf9e5f742b26305322676b09f6f393a0
PhelanUX <s4kyo4s@gmail.com>
2026-05-04 08:08:40 +0700

    8:8:44

--- Booking-hotel/src/App.jsx
+++ Booking-hotel/src/App.jsx
@@ -1,22 +1,39 @@
 import React, {useState} from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from './components/room/AddRoom'
 import ExistingRooms from "./components/room/ExistingRooms"
+import EditRoom from "./components/room/EditRoom"
+import Home from "./components/home/Home"
+import NavBar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 
 function App() {
 
   return (
-    // <>
-    //   <AddRoom/>
-    // </>
-    <Routes>
-      <Route path="/" element={<AddRoom />} />
-      <Route path="/add-room" element={<AddRoom />} />
-      <Route path="/existing-rooms" element={<ExistingRooms />} />
+
+    <main>
+    <Router>
+      <NavBar/>
+      <Routes>
+
+
+        <Route path="/" element={<Home/>} />
+        
+						<Route path="/edit-room/:roomId" element={<EditRoom />} />
+						<Route path="/existing-rooms" element={<ExistingRooms />} />
+						<Route path="/add-room" element={<AddRoom />} />
+
+
     </Routes>
+    </Router>
+    <Footer/>
+    </main>
+
+
+    
   )
 }
 
